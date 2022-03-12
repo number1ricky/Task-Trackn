8e7460c1b5fa6f0f78fe32f36ca483314872b3b8
Richard Brown <69214042+number1ricky@users.noreply.github.com>
2022-03-12 10:17:52 -0800

    nevermind

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import {Link} from 'react-router-dom'
 
 const About = () => {
   return (
     <div>
       <h4>Version 1.0.0</h4>
       <p>Routed with React Router DOM</p>
-      <a href="/">Go Back</a>
+      <h1>Thank you for coming :)</h1>
+      <Link to="/">Go Back</Link>
     </div>
   )
 }
