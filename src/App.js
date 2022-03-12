09f010b0a1a6cb80ba06fcc5b42bb22c5c74b8d1
Richard Brown <69214042+number1ricky@users.noreply.github.com>
2022-03-12 09:25:04 -0800

    added crud

--- src/App.js
+++ src/App.js
@@ -11,9 +11,10 @@ import { FaSalesforce } from 'react-icons/fa'
 const App = () => {
   const funQ = false
   const titleApp = 'Task Tracker Application'
-  const [showAddTask, setShowAddTask] = useState(false)
+  const [showAddTask, setShowAddTask] = useState(true)
   const [tasks, setTasks] = useState([])
 
+  //get tasks
   useEffect(() => {
     const getTasks = async () => {
       const tasksFromServer = await fetchTasks()
@@ -21,6 +22,7 @@ const App = () => {
     }
     getTasks()
   }, [])
+
   //fetch tasks
   const fetchTasks = async () => {
     const res = await fetch('http://localhost:3001/tasks')
@@ -28,26 +30,60 @@ const App = () => {
     return data
   }
 
+  //fetch task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:3001/tasks/${id}`)
+    const data = await res.json()
+    return data
+  }
+
 //Add task
-const addTask = (task) => {
-  const id = Math.floor(Math.random() * 10000) +1
-  const newTask = {id, ...task}
-  setTasks([...tasks, newTask])
+const addTask = async (task) => {
+  const res = await fetch('http://localhost:3001/tasks', {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(task)
+  })
+  const data = await res.json()
+  setTasks([...tasks, data])
+
+  // const id = Math.floor(Math.random() * 
+  // 10000) +1
+  // const newTask = {id, ...task}
+  // setTasks([...tasks, newTask])
 }
 
 
 // Delete Task
-const deleteTask = (id) => {
+const deleteTask = async (id) => {
+  await fetch(`http://localhost:3001/tasks/${id}`, {
+    method: 'DELETE'
+  })
   setTasks(tasks.filter((task) => task.id !==id))
 }
 
 // toggle reminder  
 
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = { ...taskToToggle, 
+    reminder: !taskToToggle.reminder}
+    const res = await fetch(`http://localhost:3001/tasks/${id}`,{
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(updTask)
+    })
+
+    const data = await res.json()
+
     setTasks(
       tasks.map((task) => 
         task.id === id ? { ...task, reminder: 
-        !task.reminder } : task
+        data.reminder } : task
       )
     )
   }
